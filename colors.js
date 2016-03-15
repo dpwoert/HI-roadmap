5d3ba4dd83ecf3cacfdb188d9be4d488f308ba20
davey <davey@dpwoert.com>
2016-03-15 20:31:44 +0000

    colors

---
+++ colors.js
@@ -0,0 +1,7 @@
+window.colors = {
+	'driver': new THREE.Color(0xff0000),
+	'aviation': new THREE.Color(0x0094ff),
+	'rail': new THREE.Color(0x0094ff),
+	'cars': new THREE.Color(0x0094ff),
+	'space': new THREE.Color(255/181,0,1)
+}
