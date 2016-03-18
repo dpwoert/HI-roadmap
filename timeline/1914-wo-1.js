f4535bb05f836bd77288ec544a66b72170e15903
davey <davey@dpwoert.com>
2016-03-18 00:20:30 +0000

    WO1 added

---
+++ timeline/1914-wo-1.js
@@ -0,0 +1,18 @@
+(function(){
+
+	var evt = new TimelineEvent(timeline);
+	var route;
+
+	evt
+		.setDate(28,6,1914)
+		.marker('First world war', 'driver')
+		.onActive(function(world){
+
+			
+
+		})
+		.onLeave(function(){
+			route.remove();
+		});
+
+}())
